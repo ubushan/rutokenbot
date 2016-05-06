c6270163f89d5695cc0e1393fa1e8edc2f2237ba
ubushan <ubushaev08@gmail.com>
2016-05-06 16:49:44 +0300

    05.05.16 -  Cleared code

--- main.py
+++ main.py
@@ -3,11 +3,13 @@ import re
 import config
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
+import smtplib
+from email.mime.text import MIMEText
 
 
 def parser(self):
     #print(self)
-    #self = self.encode('utf-8').decode('ascii', 'ignore')
+    self = self.encode('utf-8').decode('ascii', 'ignore')
     url = config.KB + "/dosearchsite.action?cql=siteSearch+~+" + '"' + self + '"' + "+and+space+%3D+" + "KB" + "+and+type+%3D+" + "page" + "&queryString=" + self
     html_doc = urlopen(url).read()
     soup = BeautifulSoup(html_doc, 'html.parser')
@@ -27,6 +29,36 @@ def parser(self):
             b = re.sub(r"\['|\']|', '", '', a)
             c = re.sub(r"\\n", '\n', b)
             d = re.sub(r"- ", '\n- ', c)
-        return """*Результаты поиска:* """ + """*'""" + self + """'\nНайдено:'""" + str(count3[0]) + """'*\n""" + d
+        return """*Результаты поиска:* """ + """*'""" + self + """'\nНайдено: '""" + str(count3[0]) + """'*\n""" + d
     else:
         return """*Результаты поиска:* """ + """*'""" + self + """'*""" + """\nНичего не найдено"""
+
+
+def mail(login, passwd, from_mail, to_mail, msg):
+    mail_login = login
+    mail_passwd = passwd
+    smtp_host = 'smtp.gmail.com'
+    smtp_port = 587
+
+    mail_from = from_mail
+    mail_to = to_mail
+    mail_subject = 'Сообщение от пользователя: ' + from_mail
+    mail_text = msg + """\r
+
+---
+Письмо было отправленно посредством RutokenBot
+"""
+
+    message = MIMEText(mail_text)
+    message['Subject'] = mail_subject
+    message['From'] = from_mail
+    message['To'] = mail_to
+    message.set_charset('utf-8')
+    print(message.as_string())
+
+    mail = smtplib.SMTP(smtp_host, smtp_port)
+    mail.ehlo()
+    mail.starttls()
+    mail.login(mail_login, mail_passwd)
+    mail.sendmail(mail_from, mail_to, message.as_string())
+    mail.close()
--- tmp/test2.py
+++ tmp/test2.py
@@ -1,16 +1,39 @@
 # -*- coding: utf-8 -*-
-from urllib.request import urlopen
-from bs4 import BeautifulSoup
-import re
+import smtplib
+from email.mime.text import MIMEText
 
-SITE = 'http://kb.rutoken.ru'
+def mail(from_mail, msg):
+    mail_login = ''
+    mail_passwd = ''
+    smtp_host = 'smtp.gmail.com'
+    smtp_port = 587
 
-def noresults(self):
-    url = SITE + "/dosearchsite.action?cql=siteSearch+~+" + '"' + self + '"' + "+and+space+%3D+" + "KB" + "&queryString=" + self
-    html_doc = urlopen(url).read()
-    soup = BeautifulSoup(html_doc, 'html.parser')
-    nores = soup.find_all('div', 'search-results-container')
-    a = re.findall(r'No results', str(nores))
-    return a[0]
+    mail_from = from_mail
+    mail_to = ''
+    mail_subject = 'Сообщение от пользователя: ' + from_mail
+    mail_text = msg + """\r
 
-print(noresults(200))
+---
+Письмо было отправленно посредством RutokenBot
+"""
+
+    message = MIMEText(mail_text)
+    message['Subject'] = mail_subject
+    message['From'] = from_mail
+    message['To'] = mail_to
+    message.set_charset('utf-8')
+    print(message.as_string())
+
+    mail = smtplib.SMTP(smtp_host, smtp_port)
+    mail.ehlo()
+    mail.starttls()
+    mail.login(mail_login, mail_passwd)
+    mail.sendmail(mail_from, mail_to, message.as_string())
+    mail.close()
+
+print('Введите Ваш электронный ящик')
+from_mail = input()
+print('Опишите свою проблему в общих чертах')
+msg = input()
+
+mail(from_mail, msg)
